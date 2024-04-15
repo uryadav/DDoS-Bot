6ce1612fbc2bd51452bce7d0117e805597ed2f43
uryadav <166624202+uryadav@users.noreply.github.com>
2024-04-15 17:03:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+const token = '6807883285:AAHAf_mVwcLNk-4DR9XsfsIgbwK8zjjudiQ';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
